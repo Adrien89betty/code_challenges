4995de7cbcd08cf7df7c799d263b89f9e076fffe
adrien leveille <adrienleveille.contact@gmail.com>
2024-05-20 18:25:41 +0200

    split string challenge ==> done

---
+++ split_string.py
@@ -0,0 +1,31 @@
+def solution(s):
+    char_list = list(s)
+    new_list = []
+
+
+    if len(char_list) % 2 == 0:
+        for i in range(len(char_list)-1):
+            if i % 2 == 0:
+                new_list.append(char_list[i] + char_list[i+1])
+
+        return new_list
+
+    else:
+        char_list.append("_")
+        for i in range(len(char_list)-1):
+            if i % 2 == 0:
+                new_list.append(char_list[i] + char_list[i+1])
+
+        return new_list
+
+# solution("abcdefg")
+
+
+
+
+
+
+
+# Convert string into a list of characters
+# Test if the number of character is even or odd.
+#
