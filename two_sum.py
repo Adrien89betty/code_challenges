24b3ce075cb9591e8682872d7774c73e653a19e8
adrien leveille <adrienleveille.contact@gmail.com>
2024-06-04 20:43:45 +0200

    Two sum finish.

--- two_sum.py
+++ two_sum.py
@@ -1,2 +1,9 @@
 def two_sum(numbers, target):
-    pass
+    complements = {}
+    for index, num in enumerate(numbers):
+        diff = target - num
+        if diff in complements:
+            return complements[diff], index
+        complements[num] = index
+    return None
+
