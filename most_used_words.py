287f1197622f1a732eaffc4a9c924cbb8e9d74fc
adrien leveille <adrienleveille.contact@gmail.com>
2024-07-23 14:37:50 +0200

    Challenge not finished yet.

--- most_used_words.py
+++ most_used_words.py
@@ -1,2 +1,17 @@
 def top_3_words(text):
-    return
+    words = text.split()
+    occ = {}
+
+    for word in words:
+        if word in occ:
+            pass
+        else:
+            occ[word] = words.count(word)
+
+    
+
+    print(occ)
+
+
+# To test if the function works.
+top_3_words("test de test et encore test de ")
